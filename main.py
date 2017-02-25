b23eb73687e8ae3236f83a1d5c29d7b2d8e58593
Wade <cheesynougats@gmail.com>
2017-02-24 23:31:51 -0600

    Add template rendering to main page and new post page, create database structure

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import re
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
+class Posts(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.StringProperty(required = True)
+    created_date = db.DateTimeProperty(auto_now_add = True)
+
 class Redir(webapp2.RequestHandler):
 
     def get(self):
@@ -17,12 +22,25 @@ class Redir(webapp2.RequestHandler):
 class Index(webapp2.RequestHandler):
 
     def get(self):
-        self.response.write("Post Index")
+        posts = db.GqlQuery("SELECT * FROM Posts ORDER BY created_date DESC LIMIT 5")
+        t = jinja_env.get_template("frontpage.html")
+        content = t.render(posts = posts, error = self.request.get("error"))
+        self.response.write(content)
 
 class NewPostHandler(webapp2.RequestHandler):
 
-    def get(self):
-        self.response.write("New Post")
+    def post(self):
+        if self.request.get("subject"):
+            new_subject = self.request.get("subject")
+        else:
+            new_subject = ""
+        if self.request.get("content"):
+            new_content = self.request.get("content")
+        else:
+            new_content = ""
+        t = jinja_env.get_template("newpost.html")
+        content = t.render(subject = new_subject, content = new_content, error = self.request.get("error"))
+        self.response.write(content)
 
 
 
